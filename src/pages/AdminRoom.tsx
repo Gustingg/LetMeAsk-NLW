ebe997bbba36784efc09406131530e37875637e2
Gustavo Cordeiro <gustavo.vilas@yahoo.com.br>
2021-09-17 16:12:14 -0300

    4839

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,13 +1,12 @@
-import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
-import { useAuth } from '../hooks/useAuth';
+//import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
-import { database } from '../services/firebase';
+
 
 import '../styles/room.scss';
 
@@ -18,45 +17,21 @@ type RoomParams = {
 
 
 export function AdminRoom(){
-    const { user } = useAuth();
+    //const { user } = useAuth();
     const params = useParams<RoomParams>();
-    const [newQuestion, setNewQuestion] = useState('');
     const roomId = params.id;
 
     const { title, questions  } = useRoom(roomId);
-
-    async function handleSendQuestion(event: FormEvent){
-        event.preventDefault();
-
-        if(newQuestion.trim() === ''){
-            return;
-        }
-
-        if(!user){
-            throw new Error('You must be logged in');
-        }
-
-        const question = {
-            content: newQuestion,
-            author: {
-                name: user?.name,
-                avatar: user.avatar
-            },
-            isHighlighted: false,
-            isAnswered: false
-        }
-
-        await database.ref(`rooms/${roomId}/questions`).push(question);
-
-        setNewQuestion('');
-    }
 
     return(
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <RoomCode code={params.id} />
+                    <div>
+                        <RoomCode code={params.id} />
+                        <Button isOutlined>Encerrar Sala</Button>
+                    </div>
                 </div>
             </header>
 
